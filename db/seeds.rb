10a8fe233b1fb3b85cb080fa3c8d602123cab574
EC2 Default User <ec2-user@ip-172-31-38-167.ap-northeast-1.compute.internal>
2021-03-24 07:29:13 +0000

    ジムアプリ DBreset 202103２４

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Reservation.create(user_id:1,lesson_id:1)
+# Reservation.create(user_id:1,lesson_id:1)
 
 Lesson.create(title:"Body Combat",level:3,category:"脂肪燃焼",started_at:"2021/02/19",description:"ボクシングの動きで脂肪燃焼燃焼を図る",price:0,instructor:"川下",limits:30)
+
+User.create(name:"Guest1",point:0,birthday:"2000/01/21",address:"Osaka*********",sex:"male",email:"guest1@example.com",password:"password",password_confirmation:"password")
+User.create(name:"Guest2",point:0,birthday:"2000/01/21",address:"Osaka*********",sex:"male",email:"guest2@example.com",password:"password",password_confirmation:"password")
+User.create(name:"Guest3",point:0,birthday:"2000/01/21",address:"Osaka*********",sex:"male",email:"guest3@example.com",password:"password",password_confirmation:"password")
+User.create(name:"Guest4",point:0,birthday:"2000/01/21",address:"Osaka*********",sex:"male",email:"guest4@example.com",password:"password",password_confirmation:"password")
