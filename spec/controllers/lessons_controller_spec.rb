dbe353308c83aca7be4c5d6d12328ddc8f406a97
EC2 Default User <ec2-user@ip-172-31-38-167.ap-northeast-1.compute.internal>
2021-04-05 07:07:32 +0000

    ジムアプリ Rspec LessonsController#showテストコード作成中 20210405

--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
-Rspec.describe LessonsController, type: :request do
-    describe "#index" do
-        context "lessons exist" do
-            get "/lessons"
-            expect(response.status).eq(200)
+RSpec.describe LessonsController, type: :request do
+    describe '#index' do
+        context 'lessons exist' do
+            it 'response success' do
+                get '/lessons'
+                expect(response).to be_success
+                expect(response.status).to eq(200)
+            end
+        end
+    end
+    describe '#show' do
+        before do
+            @lesson = FactoryBot.create(:lesson)
+        end
+        it 'response success' do
+            get '/lessons/:id',params: {id: @lesson.id}
+            expect(response).to be_success
+            expect(response.status).to eq(200)
         end
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
     describe "validations" do
-        describe "#email" do
+        describe "email" do
             it { is_expected.to validate_presence_of(:email) }
         end
-        describe "#password" do
+        describe "password" do
             it { is_expected.to validate_presence_of(:password) }
         end
-        describe "#password_confirmation" do
-            it { is_expected.to validate_presence_of(:password_confirmation) }
+    end
+    describe "password,password_confirmation" do
+        it "password == password_confirmation" do
+            user = User.new(email: 'samurai@example.com',password: 'password',password_confirmation: 'password')
+            expect(user.save).to be true
+        end
+        it "password =! password_confirmation" do
+            user = User.new(email: 'samurai@example.com',password: 'password',password_confirmation: 'miss_password')
+            expect(user.save).to be false
         end
     end
 end
