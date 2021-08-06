181077ed2e267be0fba032e6406f490c3752446d
EC2 Default User <ec2-user@ip-172-31-38-167.ap-northeast-1.compute.internal>
2021-08-06 02:06:57 +0000

    ジムアプリ 20210806　-v3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,8 +62,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   
-  config.log_level = :warn
-
+  config.log_level = :debug
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
